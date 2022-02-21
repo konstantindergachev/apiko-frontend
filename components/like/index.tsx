caf8f435b2e3d864baab2408406adb01648582b6
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2022-02-21 16:35:55 +0200

    Liked the products

--- components/like/index.tsx
+++ components/like/index.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react';
-import Image from 'next/image';
-import like from '@/images/like.svg';
-
 import styles from './styles.module.css';
 
-export const LikeButton: React.FC = (): JSX.Element => {
-  const [isLiked, setLiked] = useState({ isLiked: false });
+interface ILikeButton {
+  onClick: (id: number) => () => void;
+  productId: number;
+  isLiked: boolean;
+}
 
-  const handleClick = () => {
-    setLiked((prev) => ({ isLiked: !prev.isLiked }));
-  };
-
+export const LikeButton: React.FC<ILikeButton> = ({ onClick, productId, isLiked }): JSX.Element => {
   return (
-    <button
-      type="button"
-      className={isLiked.isLiked ? `${styles.like} ${styles.liked}` : styles.like}
-      onClick={handleClick}
-    >
-      <Image src={like} alt="like" width={18} height={18} />
+    <button type="button" className={styles.like} onClick={onClick(productId)}>
+      <div className={isLiked ? `${styles.heart} ${styles.active}` : styles.heart}></div>
     </button>
   );
 };
--- components/products/index.tsx
+++ components/products/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import { IProducts } from '@/interfaces/products';
 import { Card } from '@/components/shared/card';
@@ -6,6 +7,17 @@ import { LikeButton } from '@/components/like';
 import styles from './styles.module.css';
 
 export const Products: React.FC<IProducts> = ({ products }): JSX.Element => {
+  const [ids, setIds] = useState<number[]>([]);
+
+  const handleProductLike = (productId: number) => (): void => {
+    if (ids.includes(productId)) {
+      const filteredIds = ids.filter((id) => id !== productId);
+      setIds([...filteredIds]);
+    } else {
+      setIds([...ids, productId]);
+    }
+  };
+
   return (
     <section className={styles.products}>
       {products.map((product) => {
@@ -18,7 +30,11 @@ export const Products: React.FC<IProducts> = ({ products }): JSX.Element => {
               <p>{product.title}</p>
               <h3>$ {product.price}</h3>
             </div>
-            <LikeButton />
+            <LikeButton
+              onClick={handleProductLike}
+              productId={product.id}
+              isLiked={ids.includes(product.id)}
+            />
           </Card>
         );
       })}
